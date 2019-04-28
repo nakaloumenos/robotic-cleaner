f2a4e532bff4bcfdceb3d92d7646826cd541d11c
Nikolaos Alexandros Kaloumenos <nikolaos.kaloumenos@marshmallow.co>
2019-04-28 17:01:23 +0100

    initialize project

---
+++ src/main/java/com/example/roboticcleaner/RoboticCleanerApplication.java
@@ -0,0 +1,13 @@
+package com.example.roboticcleaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboticCleanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboticCleanerApplication.class, args);
+	}
+
+}
